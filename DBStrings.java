626c1119ee71ceec60cba3dab4e2da762b408fce
ashtonoconnor <43684340+ashtonoconnor@users.noreply.github.com>
2019-04-17 12:50:35 -0600

    Add files via upload

---
+++ DBStrings.java
@@ -0,0 +1,15 @@
+package com.example.mysqlexampleproject;
+
+public class DBStrings {
+
+
+
+    static final String DATABASE_URL = "192.168.1.110 : 3306";
+    static final String DATABASE_NAME = "fruit";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "toor";
+
+
+
+
+}
